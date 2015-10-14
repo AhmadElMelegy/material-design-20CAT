eea772d18f3b3fd2fe771ee13a432408e2ba9686
Ahmad <ahmad.melegy@gmail.com>
2015-10-15 01:13:07 +0200

    Add FAB.

--- app/src/main/java/org/catreloaded/twentycat/MainActivity.java
+++ app/src/main/java/org/catreloaded/twentycat/MainActivity.java
@@ -1,7 +1,10 @@
 package org.catreloaded.twentycat;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Add your action", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
