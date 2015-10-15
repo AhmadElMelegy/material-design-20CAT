24673704f5d7e7e605a3a879ded614268b3a58af
Ahmad <ahmad.melegy@gmail.com>
2015-10-15 13:33:14 +0200

    Add TabLayout.

--- app/src/main/java/org/catreloaded/twentycat/MainActivity.java
+++ app/src/main/java/org/catreloaded/twentycat/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,14 +12,22 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private CoordinatorLayout rootLayout;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        rootLayout = (CoordinatorLayout) findViewById(R.id.rootLayout);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
